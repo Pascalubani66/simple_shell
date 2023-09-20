a0996c3367cd59f7b009624d021bde010f4ff8a3
figgydiggy24 <riaahh20@yahoo.com>
2023-09-20 14:58:56 +0000

    betty corrrect

--- find_exec.c
+++ find_exec.c
@@ -1,38 +1,12 @@
 #include "shell.h"
+/**
+ * prnt_environ- function that will print environment variables
+ */
 
-char *_custgetenv(char *argname) 
+void prnt_environ(void)
 {
-    if (argname == NULL)
-    {
-        perror("Name is NULL");
-        return NULL;
-    }
+	char **env_ptr;
 
-    size_t argname_length = strlen(argname);
-
-    for (int m = 0; environ[m]; m++) 
-{
-	if (c_strcmp(argname, environ[m]) == 0) 
-	{
-		char *posit = c_str_chr(environ[m], '='); // Find the position of '='
-		if (posit)
-		{
-			posit++;i
-				char *val = c_strdu_p(posit);  // Move past '='
-			return val;// Allocate memory and copy the value
-		}
-		else
-		{
-			return NULL; // '=' not found, invalid environment variable
-		}
-	}
-}
-
-return NULL; // Variable not found
-}
-void prnt_environ()
-{
-	char **env_ptr = environ;
 	while (*env_ptr != NULL)
 	{
 		write(STDOUT_FILENO, *env_ptr, cust_strlen(*env_ptr));
